19c2268b754ccb0f74ee51685c2858fb6c475e21
Redkiyslowpoke <113329094+Redkiyslowpoke@users.noreply.github.com>
2023-01-24 20:52:16 +0300

    Update main.cpp

--- hash/main.cpp
+++ hash/main.cpp
@@ -3,32 +3,42 @@
 #include <fstream>
 #include <cryptopp/hex.h> 
 #include <cryptopp/sha.h> 
+#include <cryptopp/files.h> 
 
 using namespace std;
 
-int main (int argc, char* argv[])
+int main ()
 {
     using namespace CryptoPP;
     
     SHA1 hash;
 
+    /*
     ifstream f("TextToHash.txt");
     f.seekg(0,ios::end);
-    int bazepsize = f.tellg();
-    f.seekg(0,ios::beg);
-    char* buf = new char[bazepsize];
-    f.read(buf,bazepsize);
+    int fsize = f.tellg();              для сохранения
+    f.seekg(0,ios::beg);               содержимого файла
+    char* buf = new char[fsize];
+    f.read(buf,fsize);                  
     string file = string(buf);
     file.erase(file.size()-1, 1);
-    
+    f.close();
+    */
     string digest;
 
+    /*
     StringSource(file, true,             
+                 new HashFilter(hash,                           при чтении 
+                                new HexEncoder(              содержимого файла
+                                    new StringSink(digest))));
+    */
+    
+    FileSource("TextToHash.txt", true,             
                  new HashFilter(hash,       
                                 new HexEncoder(     
-                                    new StringSink(digest))));  
+                                    new StringSink(digest))));
     // cout << file << endl;  демонстрация содержимого файла
     cout << digest << endl;
-    f.close();
+
     return 0;
 }
